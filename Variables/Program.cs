7a5849c4c13df2ae1e781d9aea5047526515726f
Kristen Shea <kstar61@live.com>
2018-07-16 21:46:58 -0500

    Add project files.

---
+++ Variables/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            int x;
+            int y;
+            x = 7;
+            y = x + 3;
+            Console.WriteLine(y);
+            Console.ReadLine();
+            */
+
+            Console.WriteLine("what is your name?");
+            Console.Write("Type your first name: ");
+            String myFirstName;
+            myFirstName = Console.ReadLine();
+
+            //String myLastName;
+            //Console.Write("Type your last name: ");
+            //myLastName = Console.ReadLine();
+
+            Console.WriteLine("Type your last name: ");
+            string myLastName = Console.ReadLine();
+
+            Console.WriteLine("Hello, " + myFirstName + " " + myLastName);
+            Console.ReadLine();
+
+
+
+
+
+        }
+    }
+}
